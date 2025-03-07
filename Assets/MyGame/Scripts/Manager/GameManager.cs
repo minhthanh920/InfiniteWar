eff7442bfe550eecbe579f795ea6256f734cf78e
minhthanh920 <minhthanh9257@gmail.com>
2025-03-07 18:43:49 +0700

    Add animation

---
+++ Assets/MyGame/Scripts/Manager/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : BaseManager<GameManager>
+{
+    private void Start()
+    {
+        //if (UIManager.HasInstance)
+        //{
+        //    UIManager.Instance.ShowNotify<NotifyLoading>();
+        //    NotifyLoading scr = UIManager.Instance.GetExistNotify<NotifyLoading>();
+        //    if (scr != null)
+        //    {
+        //        scr.AnimationLoaddingText();
+        //        scr.DoAnimationLoadingProgress(5, () =>
+        //        {
+        //            UIManager.Instance.ShowScreen<ScreenHome>();
+        //            scr.Hide();
+        //        });
+        //    }
+        //}
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
